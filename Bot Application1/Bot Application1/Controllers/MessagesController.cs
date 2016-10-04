8d1146e0304ed2375e9b8af34278b1263643bdd9
Benedikt Allendorf <Benedikt.Allendorf@gmail.com>
2016-10-04 16:43:16 +0200

    fancy features

--- Bot Application1/Bot Application1/Controllers/MessagesController.cs
+++ Bot Application1/Bot Application1/Controllers/MessagesController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Bot_Application1
 {
@@ -19,6 +20,7 @@ namespace Bot_Application1
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
+            /*
             if (activity.Type == ActivityTypes.Message)
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
@@ -26,13 +28,61 @@ namespace Bot_Application1
                 int length = (activity.Text ?? string.Empty).Length;
 
                 // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+
+                Activity reply;
+                if (activity.Text == "Ben")
+                {
+                    reply = activity.CreateReply($"Cooler Name, {activity.Text}!");
+                } else
+                {
+                    reply = activity.CreateReply($"Hey, {activity.Text}!");
+                }
+               // Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
                 await connector.Conversations.ReplyToActivityAsync(reply);
             }
             else
             {
                 HandleSystemMessage(activity);
             }
+            */
+
+            Activity replyToConversation = activity.CreateReply("Should go to conversation, with a hero card");
+            replyToConversation.Recipient = activity.From;
+            replyToConversation.Type = "message";
+            replyToConversation.Attachments = new List<Attachment>();
+            List<CardImage> cardImages = new List<CardImage>();
+            cardImages.Add(new CardImage(url: "https://<ImageUrl1>"));
+            cardImages.Add(new CardImage(url: "https://<ImageUrl2>"));
+            List<CardAction> cardButtons = new List<CardAction>();
+            CardAction plButton = new CardAction()
+            {
+                Value = "https://en.wikipedia.org/wiki/Pig_Latin",
+                Type = "openUrl",
+                Title = "WikiPedia Page"
+            };
+            cardButtons.Add(plButton);
+            HeroCard plCard = new HeroCard()
+            {
+                Title = "I'm a hero card",
+                Subtitle = "Pig Latin Wikipedia Page",
+                Images = cardImages,
+                Buttons = cardButtons
+            };
+            Attachment plAttachment = plCard.ToAttachment();
+            replyToConversation.Attachments.Add(plAttachment);
+            var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+            var reply = await connector.Conversations.SendToConversationAsync(replyToConversation);
+
+            //var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+            //var replyMessage = activity.CreateReply(); // ("Yo, I heard you.", "en");
+            //replyMessage.Attachments.Add(new Attachment()
+            //{
+            //    ContentUrl = "https://upload.wikimedia.org/wikipedia/en/a/a6/Bender_Rodriguez.png",
+            //    ContentType = "image/png",
+            //    Name = "Bender_Rodriguez.png"
+            //});
+            //await connector.Conversations.ReplyToActivityAsync(replyMessage);
+
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
