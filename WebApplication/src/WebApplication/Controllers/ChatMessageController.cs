a45031cd04fc976f7eb02e24f34b2ea9ff7a34eb
mark-mit-k <warneke.mark@gmail.com>
2016-10-05 01:12:28 +0200

    Initial Commit

---
+++ WebApplication/src/WebApplication/Controllers/ChatMessageController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Logic;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+
+    public class ChatMessageController : Controller
+    {
+        [Route("api/users/{userId}/[controller]")]
+        [HttpGet]
+        public IActionResult Get(int userId)
+        {
+            User user = new UserManager().findById(userId);
+            if (user != null)
+            {
+                return Ok(user.ChatMessages);
+                
+            }
+            return NotFound(userId);
+
+        }
+
+        [Route("api/users/{userId}/[controller]")]
+        [HttpPost]
+        public IActionResult Post([FromBody] Chat Chat, int userId)
+        {
+            User user = new UserManager().findById(userId);
+            if (user != null)
+            {
+                user.ChatMessages.Add(Chat);
+                return Ok(Chat);
+            }
+            return NotFound(userId);
+
+        }
+    }
+}
---
+++ WebApplication/src/WebApplication/Controllers/DeductionController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApplication.Logic;
+using WebApplication.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication.Controllers
+{
+   
+    public class DeductionController : Controller
+    {
+        [Route("api/users/{userId}/[controller]")]
+        [HttpGet]
+        public IActionResult Get(int userId)
+        {
+            Deduction result = null;
+            User user = new UserManager().findById(userId);
+            if (user != null)
+            {
+                result = user.Deduction;
+                return Ok(result);
+            }
+            return NotFound(userId);
+
+        }
+
+        [Route("api/users/{userId}/[controller]")]
+        [HttpPost]
+        public IActionResult Post([FromBody] Deduction deduction, int userId)
+        {
+            User user = new UserManager().findById(userId);
+            if (user != null)
+            {
+                user.Deduction = deduction;
+                return Ok(deduction);
+            }
+
+            return NotFound(userId);
+          }
+    }
+}
