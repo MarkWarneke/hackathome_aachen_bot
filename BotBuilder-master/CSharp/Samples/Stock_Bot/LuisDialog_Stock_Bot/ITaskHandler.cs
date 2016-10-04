9823a64e678df67b6cb743a4007c13b77b480f4b
Benedikt Allendorf <Benedikt.Allendorf@gmail.com>
2016-10-04 22:23:38 +0200

    alles geändert

---
+++ BotBuilder-master/CSharp/Samples/Stock_Bot/LuisDialog_Stock_Bot/ITaskHandler.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis.Models;
+using System.Threading.Tasks;
+
+namespace StockLuisDlg
+{
+    internal interface ITaskHandler
+    {
+        Task<string> handle(IDialogContext context, LuisResult result);
+    }
+}
---
+++ BotBuilder-master/CSharp/Samples/Stock_Bot/LuisDialog_Stock_Bot/Luis.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockLuisDlg
+{
+    [LuisModel("82ebb193-6353-425b-bc9d-9fe139b0a967", "f37a8422bd87422a951a498bb57d4d61")]
+    [Serializable]
+    public class StockDialog : LuisDialog<object>
+    {
+        [LuisIntent("getDeductionInfo")]
+        public async Task StockPrice(IDialogContext context, LuisResult result)
+        {
+            ITaskHandler handler = new DeductionInfo();
+
+            await context.PostAsync(await handler.handle(context, result));
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("None")]
+        public async Task NoneHandler(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("I'm sorry, I don't understand");
+            context.Wait(MessageReceived);
+        }
+    }
+}
